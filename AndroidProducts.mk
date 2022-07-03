c7bdb0381dca6e1ec3e5efcd95d1d3b80bd73159
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-07-03 21:32:40 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX3031.mk
+    $(LOCAL_DIR)/arrow_RMX3031.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX3031-user \
-    lineage_RMX3031-userdebug \
-    lineage_RMX3031-eng
+    arrow_RMX3031-user \
+    arrow_RMX3031-userdebug \
+    arrow_RMX3031-eng
